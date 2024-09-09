c4ddc46915aa4e6a7266a9f14c9d4db65797a791
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-09 12:34:46 -0300

    update

--- chat/src/main/java/com/api/openai/models/ChatCompletionRequest.java
+++ chat/src/main/java/com/api/openai/models/ChatCompletionRequest.java
@@ -1,5 +1,21 @@
 package com.api.openai.models;
+import com.fasterxml.jackson.databind.ObjectMapper; 
+import com.fasterxml.jackson.annotation.JsonProperty; 
+import java.util.ArrayList;
 
 public class ChatCompletionRequest {
+    @JsonProperty("model") 
+    public String getModel() { 
+		 return this.model; } 
+    public void setModel(String model) { 
+		 this.model = model; } 
+    String model;
+    @JsonProperty("messages") 
+    public ArrayList<Message> getMessages() { 
+		 return this.messages; } 
+    public void setMessages(ArrayList<Message> messages) { 
+		 this.messages = messages; } 
+    ArrayList<Message> messages;
+    @JsonProperty("max_tokens")
 
 }
