c9c2fb1e3467e5cbb2911a3e6048ade8042c59f8
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-09 12:22:55 -0300

    update

--- chat/src/main/java/com/api/openai/models/ChatCompletionResponse.java
+++ chat/src/main/java/com/api/openai/models/ChatCompletionResponse.java
@@ -1,5 +1,14 @@
 package com.api.openai.models;
+import java.util.ArrayList;
 
 public class ChatCompletionResponse {
+    
+    public ArrayList<Choice> choices;
+    public String id;
+    public String object;
+    public int created;
+    public String model;
+    public String system_fingerprint;    
+    public Usage usage;
 
 }
