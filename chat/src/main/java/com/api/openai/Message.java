8bcaad594032878f9f311d570cdc610658ce4ba6
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-09 12:44:20 -0300

    update

--- chat/src/main/java/com/api/openai/Message.java
+++ chat/src/main/java/com/api/openai/Message.java
@@ -1,5 +1,24 @@
 package com.api.openai;
 
+import com.fasterxml.jackson.databind.ObjectMapper; 
+import com.fasterxml.jackson.annotation.JsonProperty; 
+import java.util.ArrayList;
+
 public class Message {
 
+    @JsonProperty("role") 
+    public String getRole() { 
+		 return this.role; } 
+    public void setRole(String role) { 
+		 this.role = role; } 
+    String role;
+    @JsonProperty("content") 
+    public String getContent() { 
+		 return this.content; } 
+    public void setContent(String content) { 
+		 this.content = content; } 
+    String content;
 }
+
+
+
--- chat/src/main/java/com/api/openai/models/ChatCompletionResponse.java
+++ chat/src/main/java/com/api/openai/models/ChatCompletionResponse.java
@@ -1,14 +1,51 @@
 package com.api.openai.models;
+
+import com.fasterxml.jackson.databind.ObjectMapper; 
+import com.fasterxml.jackson.annotation.JsonProperty; 
 import java.util.ArrayList;
 
 public class ChatCompletionResponse {
     
-    public ArrayList<Choice> choices;
-    public String id;
-    public String object;
-    public int created;
-    public String model;
-    public String system_fingerprint;    
-    public Usage usage;
-
+    @JsonProperty("id") 
+    public String getId() { 
+		 return this.id; } 
+    public void setId(String id) { 
+		 this.id = id; } 
+    String id;
+    @JsonProperty("object") 
+    public String getObject() { 
+		 return this.object; } 
+    public void setObject(String object) { 
+		 this.object = object; } 
+    String object;
+    @JsonProperty("created") 
+    public int getCreated() { 
+		 return this.created; } 
+    public void setCreated(int created) { 
+		 this.created = created; } 
+    int created;
+    @JsonProperty("model") 
+    public String getModel() { 
+		 return this.model; } 
+    public void setModel(String model) { 
+		 this.model = model; } 
+    String model;
+    @JsonProperty("system_fingerprint") 
+    public String getSystem_fingerprint() { 
+		 return this.system_fingerprint; } 
+    public void setSystem_fingerprint(String system_fingerprint) { 
+		 this.system_fingerprint = system_fingerprint; } 
+    String system_fingerprint;
+    @JsonProperty("choices") 
+    public ArrayList<Choice> getChoices() { 
+		 return this.choices; } 
+    public void setChoices(ArrayList<Choice> choices) { 
+		 this.choices = choices; } 
+    ArrayList<Choice> choices;
+    @JsonProperty("usage") 
+    public Usage getUsage() { 
+		 return this.usage; } 
+    public void setUsage(Usage usage) { 
+		 this.usage = usage; } 
+    Usage usage;
 }
