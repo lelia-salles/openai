e8bdc8513dfb21ff27fd5ae0d4f7dc818b1e0311
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-10 12:05:20 -0300

    update

---
+++ chat/src/main/java/com/api/openai/config/FeignConfig.java
@@ -0,0 +1,20 @@
+package com.api.openai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value; 
+
+@Configuration
+public class FeignConfig {
+
+    @value  ("${openai.api-key}")
+    private String apiKey;
+
+    @Bean    
+    protected RequestInterceptor requestInterceptor() {
+        return requestTemplate -> requestTemplate.header("Authorization", "Bearer " + apiKey);
+        return requestTemplate -> requestTemplate.header("Content-Type: application/json");
+    }
+
+}
