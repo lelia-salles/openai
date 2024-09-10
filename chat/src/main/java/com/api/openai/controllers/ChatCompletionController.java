acf4276b583531e1ed29d4256d8624a6545dfaf7
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-10 12:33:32 -0300

    update and add Chat completion Controller class

---
+++ chat/src/main/java/com/api/openai/controllers/ChatCompletionController.java
@@ -0,0 +1,10 @@
+package com.api.openai.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestCotroller
+@RequestMapping("/chat/completions")
+public class ChatCompletionController {
+
+}
