ca49d17b43abbe608c1dd255f6086fa7eaa86574
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-09-11 17:41:03 -0300

    update

--- chat/src/main/java/com/api/openai/controllers/ChatCompletionController.java
+++ chat/src/main/java/com/api/openai/controllers/ChatCompletionController.java
@@ -1,6 +1,9 @@
 package com.api.openai.controllers;
 
+import com.api.openai.models.OpenAiClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,15 @@ public class ChatCompletionController {
 
     @Value("$(openai.model)")
     private String model;
+
+    @Autowired
+    private OpenAiClient openAiClient;
+
+    public String askQuestion(RequestBody String question) {
+
+
+    }
+
 
 
 }
