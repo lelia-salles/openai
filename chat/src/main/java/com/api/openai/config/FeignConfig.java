5b17a2e943a9c4a10d9878ca5585207cdc80cf8f
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-09-11 17:33:40 -0300

    update

--- chat/src/main/java/com/api/openai/config/FeignConfig.java
+++ chat/src/main/java/com/api/openai/config/FeignConfig.java
@@ -1,20 +1,21 @@
 package com.api.openai.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
 import feign.RequestInterceptor;
-import org.springframework.beans.factory.annotation.Value; 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
-    @value  ("${openai.api-key}")
+    @Value("${openai.api-key}")
     private String apiKey;
 
     @Bean    
     protected RequestInterceptor requestInterceptor() {
-        return requestTemplate -> requestTemplate.header("Authorization", "Bearer " + apiKey);
-        return requestTemplate -> requestTemplate.header("Content-Type:", " application/json");
+        return requestTemplate -> {
+            requestTemplate.header("Authorization", "Bearer " + apiKey);
+            requestTemplate.header("Content-Type:", " application/json");
+        };
     }
-
 }
