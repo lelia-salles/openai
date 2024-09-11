0a4b4efef5dac4c734013ad1550bc85c72989487
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-09-11 16:36:42 -0300

    fix annotation RestController

--- chat/src/main/java/com/api/openai/controllers/ChatCompletionController.java
+++ chat/src/main/java/com/api/openai/controllers/ChatCompletionController.java
@@ -3,7 +3,7 @@ package com.api.openai.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestCotroller
+@RestController
 @RequestMapping("/chat/completions")
 public class ChatCompletionController {
 
